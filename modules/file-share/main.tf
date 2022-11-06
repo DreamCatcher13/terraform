100a0640926bf71bdcf480cd0b9fefa1ea0dc967
Rand <rand@localhost.localdomain>
2022-11-06 12:05:37 +0000

    rmved file-share creation

--- modules/file-share/main.tf
+++ modules/file-share/main.tf
@@ -3,14 +3,3 @@ data "azurerm_storage_account" "accnt" {
   resource_group_name = "NetworkWatcherRG"
 }
 
-resource "azurerm_storage_share" "filesh" {
-  name                 = var.fshare_name
-  storage_account_name = var.strg_acc_name
-  quota                = var.st_quota
-}
-
-resource "azurerm_storage_share_directory" "dir" {
-  name                 = var.dir_name
-  share_name           = azurerm_storage_share.filesh.name
-  storage_account_name = var.strg_acc_name
-}
--- modules/file-share/variables.tf
+++ modules/file-share/variables.tf
@@ -8,11 +8,6 @@ variable "strg_acc_name" {
   default = "rand"
 }
 
-variable "st_quota" {
-  type    = number
-  default = 5
-}
-
 variable "dir_name" {
   type    = string
   default = "bucket"
